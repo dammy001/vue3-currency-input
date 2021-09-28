53cb895cbb4c9f234231eacf7bbfd22be504249f
Damilare Anjorin <damilareanjorin1@gmail.com>
2021-09-28 18:03:36 +0100

    directive

--- index.ts
+++ index.ts
@@ -2,7 +2,7 @@ import { App, defineAsyncComponent } from 'vue';
 import VCurrency from './directive/currency';
 
 export default {
- install(app: App): void {
+ install: (app: App): void => {
   app.component(
    'vue-currency',
    defineAsyncComponent(() => import('./components/Currency')),
